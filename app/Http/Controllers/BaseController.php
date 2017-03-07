c0af396c7b237db8214c6f9ec1f1b65557174e01
Aryel Tupinambá <aryel.tupinamba@lqdi.net>
2017-03-07 18:18:29 -0300

    Added logging to handled API exceptions

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -17,11 +17,14 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Log;
 
 class BaseController extends Controller {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     protected function api_exception(\Exception $exception) {
+
+    	Log::warning('[api_exception] ' . $exception->getMessage());
 
 	    $exceptionInfo = $exception->getMessage();
 
