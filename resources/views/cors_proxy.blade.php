5e6de9af03c6d6720d55f7c2641e3e0ae1a2f321
Aryel Tupinambá <aryel.tupinamba@lqdi.net>
2017-04-24 18:51:33 -0300

    Compatibility / IE: fixed CORS page standards issues

--- resources/views/cors_proxy.blade.php
+++ resources/views/cors_proxy.blade.php
@@ -1,14 +1,22 @@
 <!DOCTYPE HTML>
-<script src="//cdn.rawgit.com/jpillora/xdomain/0.7.4/dist/xdomain.min.js"></script>
-<script>
-	xdomain.masters({
-		'http://panel.busca-ativa-escolar.dev.lqdi.net': '/*',
-		'https://panel.busca-ativa-escolar.dev.lqdi.net': '/*',
-		'http://panel.busca-ativa-escolar.local': '/*',
-		'https://panel.busca-ativa-escolar.local': '/*',
-		'http://plataforma.buscaativaescolar.org.br': '/*',
-		'https://plataforma.buscaativaescolar.org.br': '/*',
-		'http://plataforma.homolog.buscaativaescolar.org.br': '/*',
-		'https://plataforma.homolog.buscaativaescolar.org.br': '/*'
-	});
-</script>
+<html>
+	<head>
+
+		<script src="//cdn.rawgit.com/jpillora/xdomain/0.7.4/dist/xdomain.min.js"></script>
+		<script>
+			xdomain.masters({
+				'http://panel.busca-ativa-escolar.dev.lqdi.net': '/*',
+				'https://panel.busca-ativa-escolar.dev.lqdi.net': '/*',
+				'http://panel.busca-ativa-escolar.local': '/*',
+				'https://panel.busca-ativa-escolar.local': '/*',
+				'http://plataforma.buscaativaescolar.org.br': '/*',
+				'https://plataforma.buscaativaescolar.org.br': '/*',
+				'http://plataforma.homolog.buscaativaescolar.org.br': '/*',
+				'https://plataforma.homolog.buscaativaescolar.org.br': '/*'
+			});
+		</script>
+	</head>
+	<body>
+		CORS Proxy for IE9
+	</body>
+</html>
