2a6ce1c50b2f74e14fdcd222886d648c12fdeceb
Aryel Tupinambá <aryel.tupinamba@lqdi.net>
2017-04-24 18:54:55 -0300

    Compatibility / IE: added wildcards to masters

--- resources/views/cors_proxy.blade.php
+++ resources/views/cors_proxy.blade.php
@@ -7,12 +7,16 @@
 			xdomain.masters({
 				'http://panel.busca-ativa-escolar.dev.lqdi.net': '/*',
 				'https://panel.busca-ativa-escolar.dev.lqdi.net': '/*',
+				'http://*.busca-ativa-escolar.dev.lqdi.net': '/*',
 				'http://panel.busca-ativa-escolar.local': '/*',
 				'https://panel.busca-ativa-escolar.local': '/*',
+				'http://*.busca-ativa-escolar.local': '/*',
 				'http://plataforma.buscaativaescolar.org.br': '/*',
 				'https://plataforma.buscaativaescolar.org.br': '/*',
+				'http://*.buscaativaescolar.org.br': '/*',
 				'http://plataforma.homolog.buscaativaescolar.org.br': '/*',
-				'https://plataforma.homolog.buscaativaescolar.org.br': '/*'
+				'https://plataforma.homolog.buscaativaescolar.org.br': '/*',
+				'http://*.homolog.buscaativaescolar.org.br': '/*',
 			});
 		</script>
 	</head>
