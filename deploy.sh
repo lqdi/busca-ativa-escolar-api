1da812807f0b22e40d6ecef65ddcf6536e9f3718
Aryel Tupinamba <aryel.tupinamba@lqdi.net>
2018-09-19 16:43:44 -0300

    housekeeping: added deployment routine script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+git pull origin master
+composer install --no-interaction --prefer-dist --optimize-autoloader
+echo "" | sudo -S service php7.1-fpm reload
+
+if [ -f artisan ]
+then
+    php artisan migrate --force
+    php artisan cache:clear
+    php artisan queue:restart
+fi
