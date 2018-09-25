963e7a377a7e4a3f651bae16aacd3a3fe1b61816
Sandy Santos <ssantos@unicef.org>
2018-09-25 17:06:31 -0300

    added static db for schools; fixed oracle jdk version in es setup

--- setup/install-elastic-search.sh
+++ setup/install-elastic-search.sh
@@ -20,9 +20,9 @@ echo debconf shared/accepted-oracle-license-v1-1 select true | \
   sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | \
   sudo debconf-set-selections
-  
+
 echo ">> Installing Java and Elastic Search"
-apt-get -y install oracle-java7-installer elasticsearch
+apt-get -y install oracle-java8-installer elasticsearch
 
 echo ">> Java Installed"
 echo ">> Elastic Search Installed"
