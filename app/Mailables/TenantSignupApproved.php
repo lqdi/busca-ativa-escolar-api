cfca25c1114ab5884cffc94de91d27890826ddbb
Sandy Santos <ssantos@unicef.org>
2018-11-06 16:19:23 -0200

    Melhoria: corrigir texto da mensagem gerada pelo sistema #50

--- app/Mailables/TenantSignupApproved.php
+++ app/Mailables/TenantSignupApproved.php
@@ -35,7 +35,7 @@ class TenantSignupApproved extends Mailable {
 			->subject("Sua adesão foi aprovada!")
 			->greeting('Olá!')
 			->line("A adesão de {$this->signup->city->name} à Busca Ativa Escolar foi aprovada.")
-			->line('Clique no botão abaixo para configurar a plataforma de seu município.')
+			->line('Clique no botão abaixo para configurar a plataforma.')
 			->success()
 			->action('Configurar', $setupURL);
 
