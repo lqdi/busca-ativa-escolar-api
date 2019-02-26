6e8b7f92d280314b656062a1aa27c1e9f5383372
Manoel Souza <mansouza@unicef.org>
2019-02-26 17:59:24 -0300

    Inicio listagem de escolas by tenant

--- app/Http/Controllers/Resources/SchoolsController.php
+++ app/Http/Controllers/Resources/SchoolsController.php
@@ -14,14 +14,19 @@
 namespace BuscaAtivaEscolar\Http\Controllers\Resources;
 
 
+use BuscaAtivaEscolar\CaseSteps\Pesquisa;
+use BuscaAtivaEscolar\Child;
 use BuscaAtivaEscolar\Http\Controllers\BaseController;
 use BuscaAtivaEscolar\School;
 use BuscaAtivaEscolar\Search\ElasticSearchQuery;
 use BuscaAtivaEscolar\Search\Search;
 use BuscaAtivaEscolar\Serializers\SimpleArraySerializer;
 use BuscaAtivaEscolar\Transformers\SchoolSearchResultsTransformer;
+use BuscaAtivaEscolar\Transformers\SchoolTransformer;
 use BuscaAtivaEscolar\Transformers\SearchResultsTransformer;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
+use League\Fractal\Pagination\IlluminatePaginatorAdapter;
 
 class SchoolsController extends BaseController {
 
@@ -47,5 +52,40 @@ class SchoolsController extends BaseController {
 			->respond();
 
 	}
+
+	public function all_educacenso(){
+
+        $tenant_id =  $this->currentUser()->tenant->id;
+
+        //return array with schools' id from Pesquisa Model
+        $schools_array_id  = Pesquisa::query()
+            ->select('school_last_id')
+            ->whereHas('child', function ($query_child) {
+                $query_child->where('educacenso_year', '=', 2018);
+            })
+            ->where('tenant_id', $tenant_id)
+            ->groupBy('school_last_id')
+            ->pluck('school_last_id')
+            ->toArray();
+
+        $query_school = School::where('id', '!=', null)
+            ->whereIn('id', $schools_array_id);
+
+        $max = request('max', 128);
+        if($max > 128) $max = 128;
+        if($max < 16) $max = 16;
+
+        $paginator = $query_school->paginate($max);
+        $collection = $paginator->getCollection();
+
+        return fractal()
+            ->collection($collection)
+            ->transformWith(new SchoolTransformer())
+            ->serializeWith(new SimpleArraySerializer())
+            ->paginateWith(new IlluminatePaginatorAdapter($paginator))
+            ->respond();
+
+    }
+
 
 }
--- app/School.php
+++ app/School.php
@@ -35,6 +35,9 @@ class School extends Model implements Searchable {
 		'city_name',
 		'city_ibge_id',
 		'metadata',
+        'school_cell_phone',
+        'school_phone',
+        'school_email'
 	];
 
 	protected $casts = [
