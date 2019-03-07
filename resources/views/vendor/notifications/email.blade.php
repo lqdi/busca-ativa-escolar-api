8417b47b2d0d4a749eaf34f00e1c2d85863282ce
Manoel Souza <mansouza@unicef.org>
2019-03-07 11:12:25 -0300

    Correcao da URL da imagem do template do email

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -70,7 +70,7 @@ $style = [
 				<!-- Logo -->
 				<tr bgcolor="#ffffff">
 					<td style="text-align: center;">
-						<img src="http://panel.busca-ativa-escolar.dev.lqdi.net/images/logo_bae_signup.png" style="border: 0; display: block; margin: 0 auto;" />
+						<img src="https://plataforma.buscaativaescolar.org.br/images/logo_bae_signup.png" style="border: 0; display: block; margin: 0 auto;" />
 					</td>
 				</tr>
 
