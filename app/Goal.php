5785f9c411bba659e22d5339ee7c6dfea2bc34e7
Manoel Filho <manoelfilho@Manoels-MacBook-Pro.local>
2019-09-24 15:59:30 -0300

    Novo Modelo Goal para registro das metas, nova migração e mudança do modelo city

---
+++ app/Goal.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace BuscaAtivaEscolar;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Goal extends Model
+{
+    use SoftDeletes;
+
+}
