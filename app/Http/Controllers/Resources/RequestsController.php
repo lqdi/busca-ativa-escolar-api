0c88b8697e9dee23e27db1ca55f211e093e04b20
Manoel Filho <mansouza@unicef.org>
2020-03-03 17:33:55 -0300

    Envio de email de rejeicao para o solicitante

--- app/Http/Controllers/Resources/RequestsController.php
+++ app/Http/Controllers/Resources/RequestsController.php
@@ -3,9 +3,11 @@
 namespace BuscaAtivaEscolar\Http\Controllers\Resources;
 
 use BuscaAtivaEscolar\Http\Controllers\BaseController;
+use BuscaAtivaEscolar\Mail\ReopenCaseNotification;
 use BuscaAtivaEscolar\ReopeningRequests;
 use BuscaAtivaEscolar\Tenant;
 use BuscaAtivaEscolar\User;
+use Illuminate\Support\Facades\Mail;
 
 class RequestsController extends BaseController
 {
@@ -60,6 +62,24 @@ class RequestsController extends BaseController
         $request->status = ReopeningRequests::STATUS_CANCELLED;
 
         $request->save();
+
+        if( $request->requester != null) {
+
+            $msg = new ReopenCaseNotification(
+                $request->child->id,
+                $request->child->name,
+                $request->child->current_case_id,
+                null,
+                $request->requester->name,
+                \Auth::user()->name,
+                $request->id,
+                null,
+                null,
+                ReopenCaseNotification::TYPE_REJECT_REOPEN
+            );
+
+            Mail::to( $request->requester->email )->send($msg);
+        }
 
         /* */
         $user->type = User::TYPE_GESTOR_OPERACIONAL;
