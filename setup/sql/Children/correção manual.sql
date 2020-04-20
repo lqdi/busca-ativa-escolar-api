15c8affe42784c16dcd84a835dcf142e649af617
Sandy <ssantos@unicef.org>
2020-04-20 18:16:05 -0300

    Scripts - correção manual

--- setup/sql/Children/correção manual.sql
+++ setup/sql/Children/correção manual.sql
@@ -6,7 +6,6 @@
 -- interrupted      | interrupted
 -- transferred      | transferred
 -- Crianca cancelada e caso em progresso = caso recebe o status cancelado
-use base05042020prodmin;
 SET @child_status='cancelled' COLLATE utf8_unicode_ci; 
 SET @cases_status='in_progress' COLLATE utf8_unicode_ci; 
 SET @status_update='cancelled' COLLATE utf8_unicode_ci;
@@ -41,4 +40,15 @@ SET @status_update='interrupted' COLLATE utf8_unicode_ci;
 UPDATE children_cases cc
 JOIN children c ON cc.child_id = c.id 
 SET c.child_status = @status_update
-where c.child_status = @child_status and cc.case_status = @cases_status
+where c.child_status = @child_status and cc.case_status = @cases_status;
+
+SET @child_status='cancelled' COLLATE utf8_unicode_ci; 
+SET @cases_status='cancelled' COLLATE utf8_unicode_ci; 
+SET @alert_status_update='rejected' COLLATE utf8_unicode_ci;
+
+UPDATE children_cases cc
+JOIN children c ON cc.child_id = c.id
+JOIN case_steps_alerta csa ON csa.child_id = c.id 
+SET c.alert_status = @alert_status_update, csa.alert_status = @alert_status_update
+where c.alert_status = 'pending' and csa.alert_status = 'pending' and c.child_status = 'cancelled' and cc.case_status = 'cancelled';
+
