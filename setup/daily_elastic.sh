aaf6029145ea02223968033cadffaa13aad68c6a
Manoel Filho <mansouza@unicef.org>
2020-04-28 18:42:08 -0300

    Shel Script para exportar dados do elastc

---
+++ setup/daily_elastic.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+filename='tenants.txt'
+while read tenant ; do
+    echo "${tenant}"
+    es2csv -r -q '{"query":{"bool":{"must":[],"should":[],"filter":[{"bool":{"should":[{"terms":{"tenant_id":["pending"]}}]}}]}}}' -i children_daily -u localhost:9200 -o elastic_children_daily_${tenant}.csv
+done < formatted_history.txt
