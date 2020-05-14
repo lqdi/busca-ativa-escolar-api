955e3daf25c38c8bb5938133e8228d7a21ea85e3
Sandy <ssantos@unicef.org>
2020-05-14 00:25:50 -0300

    Gráfico de evolução de (re)matriculas - Script para inserir dia ausente#537 - Ferramenta com escola de data origem e destino a corrigir

--- app/Console/Commands/InserirDiaAusenteGraficoRematriculas.php
+++ app/Console/Commands/InserirDiaAusenteGraficoRematriculas.php
@@ -41,30 +41,45 @@ class InserirDiaAusenteGraficoRematriculas extends Command
     {
         set_time_limit(0);
         ini_set('memory_limit', '2G');
+        $dateOrigem = $this->ask("Informe a data de origem - formato yyyy-mm-dd");
+        $dateDestino = $this->ask("Informe a data de destino - formato yyyy-mm-dd");
 
         $this->comment('Processo de inserção iniciado!');
 
-        $sql = "SELECT * FROM daily_metrics_consolidated where date = '2020-03-30'";
+        $sql = "SELECT * FROM daily_metrics_consolidated where date = '$dateOrigem'";
         $values = DB::select($sql);
 
+        $this->deleteDateIfexist($dateDestino);
+
         foreach ($values as $value) {
-            $this->insereDados($value);
+            $this->insereDados($value, $dateDestino);
         }
 
         $this->comment('Processo de inserção Finalizado!');
     }
 
-    private function insereDados($dados)
+    private function insereDados($dados, $date)
     {
         $sql = "INSERT into daily_metrics_consolidated (tenant_id, date, region,state, city, in_observation, out_of_school, cancelled, in_school, interrupted, transferred, justified_cancelled, selo, data) 
-values ('$dados->tenant_id', '2020-03-31', '$dados->region', '$dados->state', '$dados->city', '$dados->in_observation', '$dados->out_of_school','$dados->cancelled', '$dados->in_school', '$dados->interrupted', '$dados->transferred', '$dados->justified_cancelled', '$dados->selo','$dados->data')";
+values ('$dados->tenant_id', '$date', '$dados->region', '$dados->state', '$dados->city', '$dados->in_observation', '$dados->out_of_school','$dados->cancelled', '$dados->in_school', '$dados->interrupted', '$dados->transferred', '$dados->justified_cancelled', '$dados->selo','$dados->data')";
         try {
             DB::insert($sql);
-            $this->comment($dados->tenant_id . ' atualizado ');
-            Log::info($dados->tenant_id . ' atualizado ');
+            $this->comment($dados->tenant_id . ' inserido data: ' . $date);
+            Log::info($dados->tenant_id . ' inserido data: ' . $date);
         } catch (\Exception $e) {
             $this->comment('erro ');
-            return false;
+        }
+    }
+
+    private function deleteDateIfexist($date)
+    {
+        $sql = "DELETE from daily_metrics_consolidated WHERE date = '$date'";
+        try {
+            DB::delete($sql);
+            $this->comment('data: ' . $date . ' deletada');
+            Log::info( 'data: ' . $date . ' deletada');
+        } catch (\Exception $e) {
+            $this->comment('Erro ao deletar');
         }
     }
 }
