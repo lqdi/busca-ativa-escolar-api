5195f3731e09701ccd275114be94303fc323c9bc
Sandy <ssantos@unicef.org>
2020-05-29 23:13:44 -0300

    Desativação do método de update no log

--- setup/sql/delete_active_log_notifications_updats_filds.sql
+++ setup/sql/delete_active_log_notifications_updats_filds.sql
@@ -1,4 +1,4 @@
-DELETE from activity_log where action = 'updated';
-DELETE from notifications where type = 'BuscaAtivaEscolar\Notifications\ChildUpdated';
-OPTIMIZE TABLE activity_log;
-OPTIMIZE TABLE notifications;
+-- DELETE from activity_log where action = 'updated';
+DELETE from notifications where type = 'BuscaAtivaEscolar\\Notifications\\ChildUpdated';
+-- OPTIMIZE TABLE activity_log;
+-- OPTIMIZE TABLE notifications;
