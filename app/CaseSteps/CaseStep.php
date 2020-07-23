c4acb68faadc80a7d4643a6aa0b42046b9bb953f
Sandy Santos <43251212+sandyunicef@users.noreply.github.com>
2020-07-23 11:55:20 -0300

    Revert "Testing"

--- app/CaseSteps/CaseStep.php
+++ app/CaseSteps/CaseStep.php
@@ -282,12 +282,11 @@ abstract class CaseStep extends Model {
 	 * @return array $input The actual updated fields
 	 */
 	public function setFields(array $data) {
-	    $oldObject = clone $this;
 		$input = collect($data)->only($this->stepFields)->toArray();
 		$this->fill($input);
 		$this->save();
 
-		$this->onUpdated($oldObject);
+		$this->onUpdated();
 
 		// Update search index
 		$this->child->reindex();
@@ -346,7 +345,7 @@ abstract class CaseStep extends Model {
 
 	protected function onStart($prevStep = null) {}
 	protected function onComplete() : bool { return true; }
-	protected function onUpdated($oldObject = null) {}
+	protected function onUpdated() {}
 	protected function onAssign(User $user) {}
 
 	// ------------------------------------------------------------------------
