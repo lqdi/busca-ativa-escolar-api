0ebdcf0c761c9d2a234c8d4cf38533083cc93634
Sandy <ssantos@unicef.org>
2020-07-25 18:05:12 -0300

    Migrate criação do campo e tabela

---
+++ database/migrations/2020_07_23_173942_create_classes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->uuid('id')->index();
+            $table->primary('id');
+            $table->string('name', 100);
+            $table->string('shift', 50); //período Matutino, Vespertino ou Noturno
+            $table->string('qty_enrollment', 10);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes');
+    }
+}
