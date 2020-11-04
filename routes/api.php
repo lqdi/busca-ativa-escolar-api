1d252beca063d12d21d82e8717556fe9feb8e4fb
Manoel Souza <mansouza@unicef.org>
2020-11-04 19:54:29 -0300

    teste de headers no arquivo api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
+
+
 Route::get('/versions', function() {
 	return response()->json(['available_versions' => ['v1']]);
 });
