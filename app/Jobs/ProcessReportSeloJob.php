4c41bf58eea25b382c5d52697b4001eac659f4b9
Manoel Souza <mansouza@unicef.org>
2020-11-13 09:02:30 -0300

    Mudanca temporaria relatoriö SELO

--- app/Jobs/ProcessReportSeloJob.php
+++ app/Jobs/ProcessReportSeloJob.php
@@ -27,7 +27,7 @@ class ProcessReportSeloJob implements ShouldQueue
         set_time_limit(0);
 
         $cities = [];
-        $cities_with_goal = City::has('goal')->get();
+        $cities_with_goal = City::all();
 
         foreach ($cities_with_goal as $city) {
 
