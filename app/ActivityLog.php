916ce68647e6b20fd6741cca093155677aacf0ae
Manoel Souza <mansouza@unicef.org>
2021-05-04 18:14:41 -0300

    Utilizando a tabela activity_log da base de dados padrao

--- app/ActivityLog.php
+++ app/ActivityLog.php
@@ -25,8 +25,8 @@ class ActivityLog extends Model {
 	use SoftDeletes;
 	use IndexedByUUID;
 	use TenantScopedModel;
-// Conexão externa para a tabela activity log
-    protected $connection = 'mysql2';
+    // Conexão externa para a tabela activity log
+    //protected $connection = 'mysql2';
 	protected $table = "activity_log";
 	protected $fillable = [
 		'tenant_id',
@@ -119,8 +119,6 @@ class ActivityLog extends Model {
 	 */
 	public static function writeEntry(Model $content, $action, $parameters = [], $metadata = []) {
 		$entry = new ActivityLog();
-//        $entry->setConnection('mysql2');
-
 		$entry->tenant_id = $content->tenant_id ?? null;
 		$entry->content_type= get_class($content);
 		$entry->content_id = $content->id;
