c23ebea374deb76bac27af87d19dbbaf463860aa
Manoel Souza <mansouza@unicef.org>
2021-06-29 15:28:41 -0300

    Novo metodo para complemento do JSON de retorno da API aberta das escolas. Juncao do ID do INEP com nome da escola

--- app/Http/Controllers/Resources/SchoolsController.php
+++ app/Http/Controllers/Resources/SchoolsController.php
@@ -84,7 +84,7 @@ class SchoolsController extends BaseController
 
         $results = $search->search(new School(), $query, 12);
 
-        $values =$this->includeResults($results);
+        $values =$this->includeResultsForPesquisaWash($results);
 
         return response()->json($values, 200);
     }
@@ -353,6 +353,25 @@ class SchoolsController extends BaseController
             $objet->name = $value['_source']['name'];
             $objet->ibge_id = $value['_source']['city_ibge_id'];
             $objet->city_name = $value['_source']['city_name'];
+            array_push($arrayValues, $objet);
+        }
+        return $arrayValues;
+    }
+
+    private function includeResultsForPesquisaWash($result) {
+        if(!isset($result['hits'])) return null;
+        if(!isset($result['hits']['hits'])) return null;
+        $values = $result['hits']['hits'];
+
+        $arrayValues = [];
+
+        foreach ($values as $value) {
+            $objet = new \stdClass();
+            $objet->id = $value['_id'];
+            $objet->name = $value['_source']['name'];
+            $objet->ibge_id = $value['_source']['city_ibge_id'];
+            $objet->city_name = $value['_source']['city_name'];
+            $objet->id_name = $value['_id']." - ".$value['_source']['name'];
             array_push($arrayValues, $objet);
         }
         return $arrayValues;
